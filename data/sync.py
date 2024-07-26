6bd98de4ed6d8473f29d71cf64702dbc70025a28
Anna Borzenko <annnechko@gmail.com>
2024-07-26 12:09:33 +1200

    fix python data job, $USER

--- data/sync.py
+++ data/sync.py
@@ -3,5 +3,5 @@ import pandas as pd
 print('Hi from py!')
 
 routes_data = pd.DataFrame(['test'])
-routes_data.to_csv('./data/upload/files/routes.csv')
+routes_data.to_csv('./data/upload/routes.csv')
 print('loaded')
